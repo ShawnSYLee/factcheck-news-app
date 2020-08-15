5afb8dcf13b2617c7e628e2e00eb9db5e87762ee
Shawn Lee <sylee2k@uw.edu>
2020-08-15 12:34:08 -0700

    setting up project structure

---
+++ src/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import MainView from './Views/MainView';
+import ArticleView from './Views/ArticleView';
+
+const Main = () => {
+    return (
+        <Switch>
+            <Route exact path='/' component={ MainView } />
+            <Route exact path='/' component={ ArticleView } />
+        </Switch>
+    );
+}
+
+export default Main;
---
+++ src/Views/MainView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Styles/App.css';
+
+const MainView = () => {
+
+    return (
+        <div>
+            <span>Main View</span>
+        </div>
+    )
+}
+
+export default MainView;
