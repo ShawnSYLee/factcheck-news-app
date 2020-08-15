1c0c41f9ff36268972dfa6756e20d2b311a8c5b7
Shawn Lee <sylee2k@uw.edu>
2020-08-15 14:26:09 -0700

    created new branch

---
+++ src/Components/ArticleListItem.js
@@ -0,0 +1,11 @@
+import Recat from 'react';
+import { useHistory } from 'react-router-dom';
+
+const ArticleListItem = () => {
+    return (
+        <>
+        </>
+    );
+}
+
+export default ArticleListItem;
--- src/Views/MainView.js
+++ src/Views/MainView.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
-import { Button } from 'antd';
+import { 
+    Button,
+    Input 
+} from 'antd';
 
 import '../Styles/App.css';
+
+const { Search } = Input;
 
 const MainView = () => {
     let history = useHistory();
@@ -17,6 +22,13 @@ const MainView = () => {
             <div>
                 <span>Main View</span>
             </div>
+            <Search 
+                placeholder="Search news"
+                enterButton="Search"
+                size="large"
+                 onSearch={value => console.log(value)}
+            />
+
             <Button type="primary" onClick={goToArticle}>Go to Article</Button>
         </>
     )
